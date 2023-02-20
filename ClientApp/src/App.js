91104f9ee59aed7a421d9dd4e6f01c1267424e78
Devon Trammell <dtrammell5@msn.com>
2023-02-19 18:55:30 -0800

    Created the Intro to Tasting page. Began routing in app.js

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,7 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-
+import { TastingIntro } from './components/TastingIntro';
 import './custom.css'
 
 export default class App extends Component {
@@ -16,6 +16,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/TastingIntro' component={TastingIntro} />
       </Layout>
     );
   }
---
+++ ClientApp/src/components/TastingIntro.js
@@ -0,0 +1,25 @@
+﻿import React, { Component } from 'react';
+
+export class TastingIntro extends Component {
+    static displayName = TastingIntro.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Introduction to Whiskey Tasting</h1>
+                <p>If you're new to whiskey tasting, here's the info you need to start: </p>
+                <ul>
+                    <li>Whiskey</li>
+                    <li>Glassware</li>
+                    <li>Tasting Notes</li>
+                    <li>Flavor Samples</li>
+                    <li>Nose Samples</li>
+                    <li>Water</li>
+                    <li>Palette</li>
+                    
+                </ul>
+                
+            </div>
+        );
+    }
+}
