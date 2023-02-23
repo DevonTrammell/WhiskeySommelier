c4c911279b7553f8453e9650fb3d8d712187170a
Melinda Yu <mshellowin@gmail.com>
2023-02-22 21:26:04 -0800

    build a 'about us' page, but it cannot run succeed

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,7 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-
+import { AboutUs } from './components/AboutUs';
 import './custom.css'
 
 export default class App extends Component {
@@ -16,6 +16,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/AboutUs' component={AboutUs} />
       </Layout>
     );
   }
---
+++ ClientApp/src/components/AboutUs.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class AboutUs extends Component {
+    static displayName = Home.name;
+
+    render() {
+        return (
+            <div>
+                <title>About Us</title>
+                <p>Insert Content Here</p>
+            </div>
+        );
+    }
+}
