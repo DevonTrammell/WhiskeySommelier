be116fc5c0ece7923f5475f143e5bb92b16d00a8
Tyler Ault <tyler.ault5@outlook.com>
2023-02-22 20:44:07 -0800

    Added css file to home. Updated some small things and formatting.

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
+import "./home.css";
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-      <div>
-        <h1>Whiskey Sommelier</h1>
-            <p>Your key to your whiskey tasting needs.</p>
+      <div class = "main">
+            <h1>The companion to your whiskey tasting needs.</h1>
             <p>We'll help you:</p>
         <ul>
           <li>Discover the depth of flavor profiles</li>
           <li>Gain insight into your favorite whiskey</li>
           <li>Explore the cultured side to enjoying whiskey</li>
         </ul>
-        <p></p>
 
       </div>
     );
