711c49d0f405c8320990de5a40763cdd69aa25e3
eric <ericrasmussen1024@yahoo.com>
2023-03-04 17:51:49 -0800

    Added Entity Framework Core Nuget packages and created the initial database context along with the 'Bottle' model.

---
+++ Models/Bottle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WhiskeySommelier.Models
+{
+    public partial class Bottle
+    {
+        public string Name { get; set; }
+        public string Distiller { get; set; }
+        public byte? Age { get; set; }
+        public string Barrel { get; set; }
+        public string Nose { get; set; }
+        public string Palette { get; set; }
+        public string Finish { get; set; }
+    }
+}
---
+++ Models/WhiskeySommelierContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WhiskeySommelier.Models
+{
+    public partial class WhiskeySommelierContext : DbContext
+    {
+        public WhiskeySommelierContext()
+        {
+        }
+
+        public WhiskeySommelierContext(DbContextOptions<WhiskeySommelierContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Bottle> Bottles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=ERICSMINIPC;Database=WhiskeySommelier;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Bottle>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("bottle");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.Barrel).HasColumnName("barrel");
+
+                entity.Property(e => e.Distiller).HasColumnName("distiller");
+
+                entity.Property(e => e.Finish).HasColumnName("finish");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Nose).HasColumnName("nose");
+
+                entity.Property(e => e.Palette).HasColumnName("palette");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
