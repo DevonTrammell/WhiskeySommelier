795d5d2ae5a12144fde466bcfe80312487a61c62
Tyler Ault <tyler.ault5@outlook.com>
2023-03-05 20:17:43 -0800

    Removed everything from home.css, updated global with formatting for homepage (grid system), adjusted opacity so website is easier to read. Added an experimental table to the homepage for "featured whiskey of the day," and will update it next.

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
-import "./home.css";
+/*import "./home.css";*/
 
 export class Home extends Component {
   static displayName = Home.name;
 
+ 
   render () {
-    return (
-        <div class="main">
+      return (
+        <div class="mainhome">
             <div class="grid-container">
                 <div class="grid-item grid-item-1">
                 </div>
                 <div class="grid-item grid-item-2">
-                    <h1>The companion to your whiskey tasting needs</h1>
-                    <p1>We'll help you:</p1>
+                    <div class="homeh1">
+                        <h1>The companion to your whiskey tasting needs</h1>
+                    </div>
+                    <div class="homep1">
+                        <p1>We'll help you:</p1>
+                    </div>
+                    <div class="homeul">
                         <ul>
                         <li>Discover the depth of flavor profiles</li>
                         <li>Gain insight into your favorite whiskey</li>
                         <li>Explore the cultured side to enjoying whiskey</li>
                         </ul>
+                    </div>
+
                 </div>
                 <div class="grid-item grid-item-3">
-                    <p2>Stay tuned for updates to the site!</p2>
+                    <div class="homep2">
+                        <p2>Stay tuned for updates to the site!</p2>
+                    </div>
+
                     <ul1>
                         <li>Search for your favorite whiskey</li>
                         <li>Detailed flavor analysis</li>
                     </ul1>
                 </div>
+                <div class="grid-item grid-item-4">
+                </div>
+                  <div class="grid-item grid-item-5">
+                    <table>
+                        <tr>
+                            <th1>Featured Whiskey of the Day</th1>
+                        </tr>
+                        <tr>
+                            <td>Picture goes here</td>
+                            <td>Name of Whiskey goes here.</td>
+                        </tr>
+                    </table>
+                  </div>
+
+
             </div>
       </div>
     );
