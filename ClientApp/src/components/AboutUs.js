c66c5cc7f321e2a2b443a40fe1dc39ca4ac6b9b1
melindayu <mshellowin@gmail.com>
2023-03-08 05:20:30 -0800

    changed text , navbar and background color

--- ClientApp/src/components/AboutUs.js
+++ ClientApp/src/components/AboutUs.js
@@ -14,30 +14,30 @@ export class AboutUs extends Component {
                 <br />
                 <h1>About Us</h1>
                 <br />
-                <p><b style={{ fontSize: '40px', color: 'orangered' }}>W</b>
+                <p><b style={{ fontSize: '30px', color: 'orangered' }}>W</b>
                     elcome to the Whiskey Sommelier! This site is meant to be a companion for those who want to explore the world of whiskey.
                     We are a group of energetic and ambitious graduate students majoring in computer science who want to make a guide to whiskey
                     tasting that is as informative as it is pleasant to use and look at.
                 </p>
                 <br />
-                <p ><b style={{ fontSize: '40px', color: 'orangered' }}>A</b>
+                <p ><b style={{ fontSize: '30px', color: 'orangered' }}>A</b>
                     s passionate whisky enthusiasts, we recognized the need for a comprehensive online resource dedicated to all things whisky.
                     We want to give back to society through our professional and interests. Our goal is to provide a platform where people
                     from all over the world can come together to learn, share, and explore the fascinating world of whisky.
                 </p>
                 <br />
-                <p ><b style={{ fontSize: '40px', color: 'orangered' }}>H</b>
+                <p ><b style={{ fontSize: '30px', color: 'orangered' }}>H</b>
                     ere, you will find a wealth of information about whisky, including its instruction, tasting notes,
                     and any useful background that can make the experience more meaningful and accessible to tasters of all experience levels.
                 </p>
                 <br />
-                <p ><b style={{ fontSize: '40px', color: 'orangered' }}>O</b>
+                <p ><b style={{ fontSize: '30px', color: 'orangered' }}>O</b>
                     ur team of dedicated writers and researchers are constantly adding new content to the site,
                     ensuring that there is always something new to discover. We also welcome contributions from members of the whisky community,
                     so if you have a story to share or a perspective to offer, please get in touch.
                 </p>
                 <br />
-                <p ><b style={{ fontSize: '40px', color: 'orangered' }}>T</b>
+                <p ><b style={{ fontSize: '30px', color: 'orangered' }}>T</b>
                     hank you for visiting our Whiskey Sommelier! We hope you enjoy exploring all that we have to offer and your whiskey tasting
                     journey. The key to whiskey -- with us you will taste better!</p>
                 <br />
