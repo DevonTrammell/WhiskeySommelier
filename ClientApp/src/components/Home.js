113bef43410208f38ef49fb300623c569e429bea
eric <ericrasmussen1024@yahoo.com>
2023-03-09 23:07:58 -0800

    Added a fetch method and table to display database data within the home page.

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -2,32 +2,83 @@ import React, { Component } from 'react';
 import "./home.css";
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-        <div class="main">
-            <div class="grid-container">
-                <div class="grid-item grid-item-1">
-                </div>
-                <div class="grid-item grid-item-2">
-                    <h1>The companion to your whiskey tasting needs</h1>
-                    <p1>We'll help you:</p1>
+    constructor(props) {
+        super(props);
+        this.state = {
+            whiskeys: []
+        };
+
+        this.getAllData = this.getAllData.bind(this)
+    }
+    getAllData() {
+        fetch("https://localhost:44373/api/bottles")
+            .then(res => res.json())
+            .then(
+                (whiskeys) => {
+                    this.setState({ whiskeys: whiskeys });
+                },
+                (error) => {
+                    alert(error);
+                }
+            )
+    }
+   
+    render() {
+        return (
+            <div class="main">
+                <div class="grid-container">
+                    <div class="grid-item grid-item-1">
+                    </div>
+                    <div class="grid-item grid-item-2">
+                        <h1>The companion to your whiskey tasting needs</h1>
+                        <p1>We'll help you:</p1>
                         <ul>
-                        <li>Discover the depth of flavor profiles</li>
-                        <li>Gain insight into your favorite whiskey</li>
-                        <li>Explore the cultured side to enjoying whiskey</li>
+                            <li>Discover the depth of flavor profiles</li>
+                            <li>Gain insight into your favorite whiskey</li>
+                            <li>Explore the cultured side to enjoying whiskey</li>
                         </ul>
-                </div>
-                <div class="grid-item grid-item-3">
-                    <p2>Stay tuned for updates to the site!</p2>
-                    <ul1>
-                        <li>Search for your favorite whiskey</li>
-                        <li>Detailed flavor analysis</li>
-                    </ul1>
+                    </div>
+                    <button onClick={this.getAllData}>Press Here</button>
+                    <table cellPadding="0" cellSpacing="0">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Brand</th>
+                                <th>Distiller</th>
+                                <th>Age</th>
+                                <th>Barrel</th>
+                                <th>Nose</th>
+                                <th>Pallete</th>
+                                <th>Finish</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {this.state.whiskeys.map(whiskey =>
+                                <tr>
+                                    <td>{whiskey.name}</td>
+                                    <td>{whiskey.brand}</td>
+                                    <td>{whiskey.distiller}</td>
+                                    <td>{whiskey.age}</td>
+                                    <td>{whiskey.barrel}</td>
+                                    <td>{whiskey.nose}</td>
+                                    <td>{whiskey.pallete}</td>
+                                    <td>{whiskey.finish}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                    <div class="grid-item grid-item-3">
+                        <p2>Stay tuned for updates to the site!</p2>
+                        <ul1>
+                            <li>Search for your favorite whiskey</li>
+                            <li>Detailed flavor analysis</li>
+                        </ul1>
+                    </div>
                 </div>
             </div>
-      </div>
-    );
-  }
+        );
+    }
 }
