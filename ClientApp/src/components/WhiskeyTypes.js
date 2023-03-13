138d764eba57b3500239b90db92fc3ca5c0d96f1
Devon Trammell <dtrammell5@msn.com>
2023-03-12 23:25:41 -0700

    Resctricted the types of whiskey to just bourbon to initialize the page content.

--- ClientApp/src/components/WhiskeyTypes.js
+++ ClientApp/src/components/WhiskeyTypes.js
@@ -11,20 +11,13 @@ export class WhiskeyTypes extends Component {
                         <h1 class="homeh1">Whiskey Types</h1>
                         <h2>Stay tuned for more types of whiskey!</h2>
 
+                        <br>
                         <h3>Bourbon</h3>
                         <h4>Associated Region:</h4>
                         <p>American South; Kentucky</p>
                         <h4>Characteristics:</h4>
+                        </br>
 
-                        <h3>Scotch</h3>
-                        <h4>Associated Region:</h4>
-                        <p>Scotland</p>
-                        <h4>Characteristics:</h4>
-
-                        <h3>Irish Whiskey</h3>
-                        <h4>Associated Region:</h4>
-                        <p>Ireland</p>
-                        <h4>Characteristics:</h4>
                     </div>
                 </div>
             </div>
