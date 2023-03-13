bc62930ba413a76e847aa19aa96ec0f139da35d7
Devon Trammell <dtrammell5@msn.com>
2023-03-12 23:22:50 -0700

    Created and routed a page which provides a brief overview of different types of whiskey. To be expanded upon.

---
+++ ClientApp/src/components/WhiskeyTypes.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react';
+
+export class WhiskeyTypes extends Component {
+    static displayName = WhiskeyTypes.name;
+
+    render() {
+        return (
+            <div>
+                <div class="globalWrapper">
+                    <div>
+                        <h1 class="homeh1">Whiskey Types</h1>
+                        <h2>Stay tuned for more types of whiskey!</h2>
+
+                        <h3>Bourbon</h3>
+                        <h4>Associated Region:</h4>
+                        <p>American South; Kentucky</p>
+                        <h4>Characteristics:</h4>
+
+                        <h3>Scotch</h3>
+                        <h4>Associated Region:</h4>
+                        <p>Scotland</p>
+                        <h4>Characteristics:</h4>
+
+                        <h3>Irish Whiskey</h3>
+                        <h4>Associated Region:</h4>
+                        <p>Ireland</p>
+                        <h4>Characteristics:</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
