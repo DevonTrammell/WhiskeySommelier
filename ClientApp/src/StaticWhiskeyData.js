7a12a2535d0dcd3665b7a3247e9d4e08029a4595
eric <ericrasmussen1024@yahoo.com>
2023-03-29 18:31:50 -0700

    Added static whiskey data for testing and development purposes.

---
+++ ClientApp/src/StaticWhiskeyData.js
@@ -0,0 +1,80 @@
+﻿const Whiskeys = [
+    {
+        name: "Jack Daniels",
+        distiller: "Jack Daniels",
+        age: "1",
+        barrel: "Single Barrel",
+        nose: "stinky",
+        palette: "bad",
+        finish: "harsh",
+        type: "whiskey",
+        image: 'https://media.istockphoto.com/id/504430224/photo/jack-daniels-served-with-ice-ball.jpg?s=612x612&w=0&k=20&c=wS0iLXZ3Zpx50ZyimNyQbU6Eunqj89pTRXTu-OGLwW8='
+    },
+    {
+        name: "Jim Beam",
+        distiller: "Jim Beam",
+        age: "1",
+        barrel: "Multiple Barrel",
+        nose: "strong",
+        palette: "smooth",
+        finish: "pleasent",
+        type: "whiskey", 
+        image: 'https://media.istockphoto.com/id/496314094/photo/bottle-of-jim-beam-bourbon.jpg?s=612x612&w=0&k=20&c=7jV6ztb-84qj5nclaE4fmNdr0REegK_tQGwJxsur60U='
+    },
+    {
+        name: "Jameson",
+        distiller: "Jameson",
+        age: "1",
+        barrel: "Multiple Barrel",
+        nose: "strong",
+        palette: "smooth",
+        finish: "pleasent",
+        type: "whiskey", 
+        image: 'https://t3.ftcdn.net/jpg/04/88/53/86/360_F_488538681_Az9ns6xhwTAvsmHyw8TIJqN4Ep0CtFSl.jpg'
+    },
+    {
+        name: "Wild Turkey",
+        distiller: "Wild Turkey",
+        age: "3",
+        barrel: "Single Barrel",
+        nose: "sweet",
+        palette: "smooth",
+        finish: "sharp",
+        type: "bourbon",
+        image: 'https://thumbs.dreamstime.com/b/poznan-pol-mar-bottle-wild-turkey-brand-kentucky-straight-bourbon-whiskey-produced-austin-nichols-division-116554687.jpg'
+    },
+    {
+        name: "Makers Mark",
+        distiller: "Makers Mark",
+        age: "3",
+        barrel: "Oak Barrel",
+        nose: "sharp",
+        palette: "smooth",
+        finish: "pleasent",
+        type: "whiskey", 
+        image: 'https://st3.depositphotos.com/1063437/34691/i/600/depositphotos_346918778-stock-photo-poznan-pol-jan-2020-bottle.jpg'
+    },
+    {
+        name: "Johnnie Walker",
+        distiller: "Johnnie Walker",
+        age: "5",
+        barrel: "Oak Barrel",
+        nose: "sweet",
+        palette: "smooth",
+        finish: "sharp",
+        type: "whiskey", 
+        image: 'https://us.123rf.com/450wm/monticello/monticello1710/monticello171000182/87317386-poznan-poland-aug-24-2017-johnnie-walker-is-the-most-widely-distributed-brand-of-blended-scotch.jpg?ver=6'
+    },
+    {
+        name: "Glenfiddich",
+        distiller: "Glenfiddich",
+        age: "21",
+        barrel: "Rum Cask",
+        nose: "sweet",
+        palette: "smooth",
+        finish: "sharp",
+        type: "whiskey", 
+        image:'https://robbreport.com/wp-content/uploads/2020/03/glenfiddich-21-year-old-1.jpg'
+    }
+]
+export default Whiskeys;
