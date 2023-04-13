479009653bb202d87c818ddab58f9145ad837556
eric <ericrasmussen1024@yahoo.com>
2023-04-12 20:55:55 -0700

    Created the WhiskeyDataDisplay.

---
+++ ClientApp/src/components/WhiskeyDataDisplay.js
@@ -0,0 +1,28 @@
+﻿import React, { Component } from "react";
+import '../custom.css';
+import '../Global.css';
+
+
+export class WhiskeyDataDisplay extends React.Component {
+    render() {
+        return (
+            <div className="whiskey_data_display_container">
+                <h1 className="homeh1">{this.props.name}</h1>
+
+                <h1 className="homeh1">Cask</h1>
+
+                <p>Bourbon, Refill, and Recharred casks.</p>
+                
+                <h1 className="homeh1">Nose</h1>
+                <p>{this.props.nose}</p>
+
+                <h1 className="homeh1">Palette</h1>
+                <p>{this.props.palette}</p>
+
+                <h1 className="homeh1">Finish</h1>
+                <p>{this.props.finish}</p>
+                <img className="card-image" src={this.props.img} />
+            </div>
+        )
+    }
+}
